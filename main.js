6faf18d06ed6b61bb9b4906b2b492a897dd3b9c7
Jack Preston <je.preston20@gmail.com>
2022-01-22 19:14:24 +0000

    Add countdown to complete chores

--- main.js
+++ main.js
@@ -11,30 +11,32 @@ var today = new Date(Date.now());
 daysInWeek1 = [1, 2, 3, 4, 5, 6, 7];
 daysInWeek2 = [8, 9, 10, 11, 12, 13, 14];
 daysInWeek3 = [15, 16, 17, 18, 19, 20, 21];
-daysInWeek4 = [22, 23, 24, 25, 26, 27, 0];
+daysInWeek4 = [22, 23, 24, 25, 26, 27, 28];
 
 var startWeek1 = daysInWeek1[0];
 var startWeek2 = daysInWeek2[0];
 var startWeek3 = daysInWeek3[0];
 var startWeek4 = daysInWeek4[0];
 
-// Rota is on a 4 week cycle, so (7 * 4) days in each cycle
-var daysInCycle = 7 * 4;
+function getDaysSinceCycleStart (startDate) {
+    // Rota is on a 4 week cycle, so (7 * 4) days in each cycle
+    var daysInCycle = 7 * 4;
 
-// JavaScript stores date objects as milliseconds, so will need number of milliseconds in a day
-var millisecondsInADay = 1000 * 60 * 60 * 24;
+    // JavaScript stores date objects as milliseconds, so will need number of milliseconds in a day
+    var millisecondsInADay = 1000 * 60 * 60 * 24;
 
-// Create text for footer showing today's date 
-var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-footerPara.innerHTML = "Today's date: " + today.toLocaleDateString('en-GB', dateOptions);
-
-function getWeekInCycle (date) {
-
-    dayDiff = Math.floor((today - date) / millisecondsInADay);
+    dayDiff = Math.floor((today - startDate) / millisecondsInADay);
 
     // Rota completes a cycle in 4 weeks. 7 * 4 = number of days in a cycle.
     // dayDiff % (7 * 4) is therefore number of complete days since current cycle started.
     daysSinceCycleStart = dayDiff % daysInCycle;
+
+    return daysSinceCycleStart;
+}
+
+daysSinceCycleStart = getDaysSinceCycleStart(cycleStartDate);
+
+function getWeekInCycle (daysSinceCycleStart) {
 
     if (daysSinceCycleStart >= startWeek1 && daysSinceCycleStart < startWeek2) {
         cycleWeek = 1;
@@ -76,6 +78,26 @@ function getLastWeekInCycle (week) {
 }
 
 var lastWeekInCycle = getLastWeekInCycle(weekInCycle);
+
+function getDaysLeftInCycle (weekInCycle, daysSinceCycleStart) {
+    var index = window['daysInWeek' + weekInCycle].indexOf(daysSinceCycleStart);
+    var daysLeft = 7 - index;
+    return daysLeft;
+}
+
+daysLeftInCycle = getDaysLeftInCycle(weekInCycle, daysSinceCycleStart);
+
+function getCountDownString (daysLeftInCycle) {
+    if (daysLeftInCycle == 1) {
+        return '<span style="color: #F4511E; font-weight: bold;">Last day</span> to complete chores!';
+    } else if (daysLeftInCycle == 2) {
+        return '<span style="color: #F4511E; font-weight: bold;">1 day left</span> after today to complete chores.';
+    } else {
+        return daysLeftInCycle - 1 + ' days left after today to complete chores.';
+    }
+}
+
+footerPara.innerHTML = getCountDownString(daysLeftInCycle);
 
 // Create empty rooms array. Each room will be added to the array by the room constructor.
 var rooms = [];
